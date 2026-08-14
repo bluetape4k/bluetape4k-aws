db26810f33a81a5dc7141753e17e9ae396c9dc20
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2026-08-14 22:46:14 +0900

    compileOnly 서비스 표면을 외부 consumer compile gate로 고정한다 (#511)
    대표 Java/Kotlin SDK 서비스 fixture와 publication metadata guard를 root check에 연결하고, 누락 서비스 SDK를 의도적으로 제거하는 compile 입력을 제공한다.
    
    Constraint: AWS 서비스 SDK는 consumer runtime 소유의 compileOnly 계약을 유지한다.
    Rejected: emulator 기반 통합 테스트를 fixture gate로 사용 | 컴파일 계약에는 네트워크와 컨테이너가 필요하지 않다.
    Confidence: high
    Scope-risk: narrow
    Directive: 새 compileOnly 서비스는 대표 fixture와 omission 검증을 함께 갱신한다.
    Tested: fixture GREEN, scheduler omission RED, publication ownership, Detekt, explicit-socket full check
    Not-tested: CI와 GitHub PR review

---
+++ aws-java/src/consumerFixture/kotlin/io/bluetape4k/aws/consumer/JavaServiceConsumerFixture.kt
@@ -0,0 +1,49 @@
+package io.bluetape4k.aws.consumer
+
+import io.bluetape4k.aws.cloudwatch.cloudWatchClient
+import io.bluetape4k.aws.dynamodb.dynamoDbClient
+import io.bluetape4k.aws.kinesis.kinesisClient
+import io.bluetape4k.aws.kms.kmsClient
+import io.bluetape4k.aws.s3.S3ClientFactory
+import io.bluetape4k.aws.ses.sesClient
+import io.bluetape4k.aws.sns.snsClient
+import io.bluetape4k.aws.sqs.sqsClient
+import io.bluetape4k.aws.sts.stsClient
+import software.amazon.awssdk.services.cloudwatch.CloudWatchClient
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.kinesis.KinesisClient
+import software.amazon.awssdk.services.kms.KmsClient
+import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.scheduler.SchedulerClient
+import software.amazon.awssdk.services.ses.SesClient
+import software.amazon.awssdk.services.sns.SnsClient
+import software.amazon.awssdk.services.sqs.SqsClient
+import software.amazon.awssdk.services.sts.StsClient
+
+/**
+ * Java SDK wrapper의 대표 compileOnly 서비스 표면을 외부 consumer 관점에서 확인합니다.
+ *
+ * 이 fixture는 실행하지 않고 컴파일만 하므로 AWS client를 생성하지 않습니다.
+ */
+fun javaServiceConsumerFixture(): List<Any> = listOf(
+    S3Client::class.java,
+    DynamoDbClient::class.java,
+    SnsClient::class.java,
+    SqsClient::class.java,
+    KmsClient::class.java,
+    SesClient::class.java,
+    CloudWatchClient::class.java,
+    KinesisClient::class.java,
+    SchedulerClient::class.java,
+    StsClient::class.java,
+    { S3ClientFactory.Sync.create { } },
+    { dynamoDbClient { } },
+    { snsClient { } },
+    { sqsClient { } },
+    { kmsClient { } },
+    { sesClient { } },
+    { cloudWatchClient { } },
+    { kinesisClient { } },
+    { SchedulerClient.builder().build() },
+    { stsClient { } },
+)
---
+++ aws-kotlin/src/consumerFixture/kotlin/io/bluetape4k/aws/kotlin/consumer/KotlinServiceConsumerFixture.kt
@@ -0,0 +1,49 @@
+package io.bluetape4k.aws.kotlin.consumer
+
+import aws.sdk.kotlin.services.cloudwatch.CloudWatchClient
+import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
+import aws.sdk.kotlin.services.kinesis.KinesisClient
+import aws.sdk.kotlin.services.kms.KmsClient
+import aws.sdk.kotlin.services.s3.S3Client
+import aws.sdk.kotlin.services.scheduler.SchedulerClient
+import aws.sdk.kotlin.services.ses.SesClient
+import aws.sdk.kotlin.services.sns.SnsClient
+import aws.sdk.kotlin.services.sqs.SqsClient
+import aws.sdk.kotlin.services.sts.StsClient
+import io.bluetape4k.aws.kotlin.cloudwatch.cloudWatchClientOf
+import io.bluetape4k.aws.kotlin.dynamodb.dynamoDbClientOf
+import io.bluetape4k.aws.kotlin.kinesis.kinesisClientOf
+import io.bluetape4k.aws.kotlin.kms.kmsClientOf
+import io.bluetape4k.aws.kotlin.s3.s3ClientOf
+import io.bluetape4k.aws.kotlin.ses.sesClientOf
+import io.bluetape4k.aws.kotlin.sns.snsClientOf
+import io.bluetape4k.aws.kotlin.sqs.sqsClientOf
+import io.bluetape4k.aws.kotlin.sts.stsClientOf
+
+/**
+ * AWS Kotlin SDK wrapper의 대표 compileOnly 서비스 표면을 외부 consumer 관점에서 확인합니다.
+ *
+ * 이 fixture는 실행하지 않고 컴파일만 하므로 AWS client를 생성하지 않습니다.
+ */
+fun kotlinServiceConsumerFixture(): List<Any> = listOf(
+    S3Client::class.java,
+    DynamoDbClient::class.java,
+    SnsClient::class.java,
+    SqsClient::class.java,
+    KmsClient::class.java,
+    SesClient::class.java,
+    CloudWatchClient::class.java,
+    KinesisClient::class.java,
+    SchedulerClient::class.java,
+    StsClient::class.java,
+    { s3ClientOf() },
+    { dynamoDbClientOf(region = "ap-northeast-2") },
+    { snsClientOf() },
+    { sqsClientOf() },
+    { kmsClientOf() },
+    { sesClientOf() },
+    { cloudWatchClientOf() },
+    { kinesisClientOf() },
+    { SchedulerClient { } },
+    { stsClientOf() },
+)
